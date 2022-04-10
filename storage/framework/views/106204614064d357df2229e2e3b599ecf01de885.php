c392f57c975b83362f0b537d68353397e7561069
Vunnam Bramham <73174236+BramhamChowdary48@users.noreply.github.com>
2022-04-10 17:32:13 +0530

    Add files via upload

---
+++ storage/framework/views/106204614064d357df2229e2e3b599ecf01de885.php
@@ -0,0 +1,12 @@
+<div class="main-panel">
+
+          <!-- content-wrapper ends -->
+          <!-- partial:partials/_footer.html -->
+          <footer style="padding-top: 600px" class="footer">
+            <div class="d-sm-flex justify-content-center justify-content-sm-between">
+              <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © Vunnam Bramham</span>
+
+            </div>
+          </footer>
+</div>
+<?php /**PATH C:\xampp\htdocs\ClothKit\resources\views/admin/body.blade.php ENDPATH**/ ?>
